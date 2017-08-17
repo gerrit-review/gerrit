d970a350fb65f969f2f451fc8606474d6b81a265
Becky Siegel <beckysiegel@google.com>
2017-08-17 16:13:29 -0700

    Project Access
    Change-Id: Ib056813236ced2fd5380f96d3aea6dee18e29caf

--- polygerrit-ui/app/scripts/util.js
+++ polygerrit-ui/app/scripts/util.js
@@ -59,5 +59,28 @@
     return '\u2026/' + pathPieces[pathPieces.length - 1];
   };
 
+  util.toSortedArray = function(obj) {
+    return util.toArray(obj).sort((a, b) => {
+      return a.id > b.id;
+    });
+  };
+
+  util.toArray= function(obj) {
+    return Object.keys(obj).map( key => {
+      return {
+        id: key,
+        value: obj[key],
+      };
+    });
+  },
+
+  // util.toObject = function(arr) {
+  //   const obj = {};
+  //   for (const item of arr) {
+  //     obj[item.id] = item.value;
+  //   }
+  //   return obj;
+  // },
+
   window.util = util;
 })(window);
